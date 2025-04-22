bc277865fa9ca484091773b6a2800e69d20b7eec
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-04-22 11:28:28 +0800

    Merge ts file conflict Angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DxButtonModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -33,6 +14,5 @@ import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
   ],
   providers: [],
   bootstrap: [AppComponent]
->>>>>>> b023638 (Added Angular implementation)
 })
 export class AppModule { }
