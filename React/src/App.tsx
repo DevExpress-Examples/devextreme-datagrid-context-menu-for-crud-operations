b4ac133667f1daab298ff256cb89466ded884380
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-05-02 15:27:02 +0800

    Add React implementation in v24.2.5

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,64 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import DataGrid, {
+  Column, Paging, Pager, Editing,
+} from 'devextreme-react/data-grid';
+import { employees } from './data';
+
+const allowedPageSizes = [5, 10, 20];
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+  const onContextMenuPreparing = useCallback((e: any): void => {
+    if (e.row.rowType === 'data') {
+      e.items = [
+        {
+          text: 'edit',
+          onItemClick(): void {
+            e.component.editRow(e.row.rowIndex);
+          },
+        },
+        {
+          text: 'insert',
+          onItemClick(): void {
+            e.component.addRow();
+          },
+        },
+        {
+          text: 'delete',
+          onItemClick(): void {
+            e.component.deleteRow(e.row.rowIndex);
+          },
+        },
+      ];
+    }
+  }, []);
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+    <div>
+      <DataGrid
+        id="gridContainer"
+        dataSource={employees}
+        keyExpr="ID"
+        onContextMenuPreparing={onContextMenuPreparing}
+      >
+        <Column dataField="FirstName" />
+        <Column dataField="LastName" />
+        <Column dataField="Title" />
+        <Column dataField="City" />
+        <Column dataField="Country" />
+
+        <Paging pageSize={10} />
+        <Pager
+          visible={true}
+          allowedPageSizes={allowedPageSizes}
+          showPageSizeSelector={true} />
+
+        <Editing
+          mode="row"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true} />
+      </DataGrid>
     </div>
   );
 }
