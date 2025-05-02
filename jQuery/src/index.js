fc223360e6efccf8f74ddea49f64b51ddf1f86f0
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-05-02 15:31:28 +0800

    Add jQuery implementation in v24.2.5

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,69 @@
-$(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+$(()=>{
+
+  var employees = [{
+    "ID": 1,
+    "FirstName": "John",
+    "LastName": "Heart",
+    "City": "Los Angeles"
+  }, {
+      "ID": 2,
+      "FirstName": "Olivia",
+      "LastName": "Peyton",
+      "City": "Los Angeles"
+  }, {
+      "ID": 3,
+      "FirstName": "Robert",
+      "LastName": "Reagan",
+      "City": "Bentonville"
+  }, {
+      "ID": 4,
+      "FirstName": "Greta",
+      "LastName": "Sims",
+      "City": "Boise"
+  }, {
+      "ID": 5,
+      "FirstName": "Brett",
+      "LastName": "Wade",
+      "City": "Atlanta"
+  }];
+
+  $("#gridContainer").dxDataGrid({
+    dataSource: employees,
+    columns: ['FirstName', 'LastName', 'Title', 'City', 'Country'],
+    paging: {
+        pageSize: 10
     },
+    pager: {
+        showPageSizeSelector: true,
+        allowedPageSizes: [5, 10, 20]
+    },
+    editing: {
+        mode: "row",
+        allowUpdating: true,
+        allowDeleting: true,
+        allowAdding: true
+    },
+    onContextMenuPreparing: function (e) {
+      if (e.row.rowType === "data") {
+          e.items = [{
+              text: "edit",
+              onItemClick: function () {
+                  e.component.editRow(e.row.rowIndex);
+              }
+          },
+          {
+              text: "insert",
+              onItemClick: function () {
+                  e.component.addRow();
+              }
+          },
+          {
+              text: "delete",
+              onItemClick: function () {
+                  e.component.deleteRow(e.row.rowIndex);
+              }
+          }];
+      }
+    }
   });
-});
+})
