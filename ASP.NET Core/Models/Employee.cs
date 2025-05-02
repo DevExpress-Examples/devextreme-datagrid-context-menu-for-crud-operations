e4ff36bdbb6ccb1b599ec427eb4405a63068a50b
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-05-02 15:13:57 +0800

    Add AspNetCore implementation in v24.2.5

---
+++ ASP.NET Core/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ ASP.NET Core/Models/EmployeesData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Core.Models
+{
+    static class EmployeesData
+    {
+        public static List<Employee> Employees = new List<Employee>() {
+            new Employee { ID = 1, FirstName = "John", LastName = "Heart", City = "Los Angeles" },
+            new Employee { ID = 2, FirstName = "Olivia", LastName = "Peyton", City = "Los Angeles" },
+            new Employee { ID = 3, FirstName = "Robert", LastName = "Reagan", City = "Bentonville" },
+            new Employee { ID = 4, FirstName = "Greta", LastName = "Sims", City = "Boise" },
+            new Employee { ID = 5, FirstName = "Brett", LastName = "Wade", City = "Atlanta" }
+        };
+    }
+}
