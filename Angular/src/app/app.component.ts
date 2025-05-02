9114d793332ecbe039a3f15e375fa0d11fec616e
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-05-02 14:41:46 +0800

    Add Angular implementation in v24.2.5

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-})
-export class AppComponent {
-  title = 'Angular';
-
-  counter = 0;
-
-  buttonText = 'Click count: 0';
-
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
-  }
-}
-=======
 import { Component } from "@angular/core";
 import { Service } from "./app.service";
 
@@ -64,4 +42,3 @@ export class AppComponent {
   }
   
 }
->>>>>>> b023638 (Added Angular implementation)
