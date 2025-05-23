9db1128a0a17a4373c4c21f2889d15a46c57ff1f
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-05-23 15:44:24 +0800

    Update Angular event type

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Service } from "./app.service";
+import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
 
 @Component({
   selector: 'app-root',
@@ -11,29 +12,30 @@ export class AppComponent {
   employees: any;
   readonly allowedPageSizes = [5, 10, 20];
 
-  onContextMenuPreparing(e: any){
-    if (e.row.rowType === "data") {
-      e.items = 
-        [
-          {
-            text: "edit",
-            onItemClick: function () {
-              e.component.editRow(e.row.rowIndex);
-            }
+  onContextMenuPreparing(e: DxDataGridTypes.ContextMenuPreparingEvent){
+    if (e.row?.rowType === 'data') {
+      const rowIndex = e.row.rowIndex;
+      if (e.rowIndex === undefined) return;
+      e.items = [
+        {
+          text: 'edit',
+          onItemClick(): void {
+            e.component.editRow(rowIndex);
           },
-          {
-            text: "insert",
-            onItemClick: function () {
-              e.component.addRow();
-            }
+        },
+        {
+          text: 'insert',
+          onItemClick: async(): Promise<void> => {
+            await e.component.addRow();
           },
-          {
-            text: "delete",
-            onItemClick: function () {
-              e.component.deleteRow(e.row.rowIndex);
-            }
-          }
-        ];
+        },
+        {
+          text: 'delete',
+          onItemClick(): void {
+            e.component.deleteRow(rowIndex);
+          },
+        },
+      ];
     }
   }
 
