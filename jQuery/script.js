f3ff718d81db4c1fa9718525ce86790f79d53908
Charles Vincent Cordial <charles.cordial@devsoft.ph>
2025-06-02 17:54:19 +0800

    Fix lint errors in jQuery project

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,69 +1,68 @@
-$(function () {
+$(() => {
+  const employees = [{
+    'ID': 1,
+    'FirstName': 'John',
+    'LastName': 'Heart',
+    'City': 'Los Angeles',
+  }, {
+    'ID': 2,
+    'FirstName': 'Olivia',
+    'LastName': 'Peyton',
+    'City': 'Los Angeles',
+  }, {
+    'ID': 3,
+    'FirstName': 'Robert',
+    'LastName': 'Reagan',
+    'City': 'Bentonville',
+  }, {
+    'ID': 4,
+    'FirstName': 'Greta',
+    'LastName': 'Sims',
+    'City': 'Boise',
+  }, {
+    'ID': 5,
+    'FirstName': 'Brett',
+    'LastName': 'Wade',
+    'City': 'Atlanta',
+  }];
 
-    var employees = [{
-        "ID": 1,
-        "FirstName": "John",
-        "LastName": "Heart",
-        "City": "Los Angeles"
-    }, {
-        "ID": 2,
-        "FirstName": "Olivia",
-        "LastName": "Peyton",
-        "City": "Los Angeles"
-    }, {
-        "ID": 3,
-        "FirstName": "Robert",
-        "LastName": "Reagan",
-        "City": "Bentonville"
-    }, {
-        "ID": 4,
-        "FirstName": "Greta",
-        "LastName": "Sims",
-        "City": "Boise"
-    }, {
-        "ID": 5,
-        "FirstName": "Brett",
-        "LastName": "Wade",
-        "City": "Atlanta"
-    }];
-
-    $("#gridContainer").dxDataGrid({
-        dataSource: employees,
-        columns: ['FirstName', 'LastName', 'Title', 'City', 'Country'],
-        paging: {
-            pageSize: 10
+  $('#gridContainer').dxDataGrid({
+    dataSource: employees,
+    columns: ['FirstName', 'LastName', 'Title', 'City', 'Country'],
+    paging: {
+      pageSize: 10,
+    },
+    pager: {
+      showPageSizeSelector: true,
+      allowedPageSizes: [5, 10, 20],
+    },
+    editing: {
+      mode: 'row',
+      allowUpdating: true,
+      allowDeleting: true,
+      allowAdding: true,
+    },
+    onContextMenuPreparing(e) {
+      if (e.row.rowType === 'data') {
+        e.items = [{
+          text: 'edit',
+          onItemClick() {
+            e.component.editRow(e.row.rowIndex);
+          },
         },
-        pager: {
-            showPageSizeSelector: true,
-            allowedPageSizes: [5, 10, 20]
+        {
+          text: 'insert',
+          onItemClick() {
+            e.component.addRow();
+          },
         },
-        editing: {
-            mode: "row",
-            allowUpdating: true,
-            allowDeleting: true,
-            allowAdding: true
-        },
-        onContextMenuPreparing: function (e) {
-            if (e.row.rowType === "data") {
-                e.items = [{
-                    text: "edit",
-                    onItemClick: function () {
-                        e.component.editRow(e.row.rowIndex);
-                    }
-                },
-                {
-                    text: "insert",
-                    onItemClick: function () {
-                        e.component.addRow();
-                    }
-                },
-                {
-                    text: "delete",
-                    onItemClick: function () {
-                        e.component.deleteRow(e.row.rowIndex);
-                    }
-                }];
-            }
-        }
-    });
+        {
+          text: 'delete',
+          onItemClick() {
+            e.component.deleteRow(e.row.rowIndex);
+          },
+        }];
+      }
+    },
+  });
 });
