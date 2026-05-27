97f662db96fa4db259c0d7d3f91fe9c019a65a68
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-27 13:58:49 +0300

    Migrate Angular: update to v21, standalone components, flat ESLint config

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import {DxDataGridTypes, DxDataGridModule} from 'devextreme-angular/ui/data-grid';
 import { Service } from './app.service';
 
 @Component({
   selector: 'app-root',
+  imports: [DxDataGridModule],
   providers: [Service],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
